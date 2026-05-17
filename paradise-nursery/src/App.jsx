38ab3d617fdb1a26a3ca98d0b289e6677ef2db78
Kone Tshivhinda <erictshivhinda@gmail.com>
2026-05-17 17:36:47 +0200

    Implement main application structure with routing for landing page, product list, and cart

---
+++ paradise-nursery/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import ProductList from './components/ProductList';
+import CartItem from './components/CartItem';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
